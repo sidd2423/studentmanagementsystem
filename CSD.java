a448a52811507ca43ccfc163d9c68b20c75a85a7
sidd2423 <112332747+sidd2423@users.noreply.github.com>
2023-10-09 11:18:25 -0400

    Update CSD.java

--- CSD.java
+++ CSD.java
@@ -3,31 +3,6 @@ package studentmanagement;
 
 
 import java.util.*;
-/* PLEASE DO NOT MODIFY A SINGLE STATEMENT IN THE TEXT BELOW.
-
-
-READ THE FOLLOWING CAREFULLY AND FILL IN THE GAPS
-
-I hereby declare that all the work that was required to 
-solve the following problem including designing the algorithms
-and writing the code below, is solely my own and that I received
-no help in creating this solution and I have not discussed my solution 
-with anybody. I affirm that I have read and understood
-the Senate Policy on Academic honesty at 
-https://secretariat-policies.info.yorku.ca/policies/academic-honesty-senate-policy-on/
-and I am well aware of the seriousness of the matter and the penalties that I will face as a 
-result of committing plagiarism in this assignment.
-
-BY FILLING THE GAPS,YOU ARE SIGNING THE ABOVE STATEMENTS.
-
-Full Name: Siddhrajsinh Parmar
-Student Number: 219091321
-Course Section: E
- */
-
-
-/*CSD is the the class for CS Department
- */
 public class CSD {
 	private ChairPerson chair;
 	private List<Faculty> faculties = new ArrayList<Faculty>();
